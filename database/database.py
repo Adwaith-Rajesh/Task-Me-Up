e70ea19eeb0cb34abfb334a2eeb3919faf98152c
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-03-29 19:24:34 +0530

    Added logging

--- database/database.py
+++ database/database.py
@@ -1,12 +1,52 @@
 import os
+
+# import logging
 from pathlib import Path
+from typing import Dict, List, Union
 
 from pysondb import db
 
+from meta.logger import Logger, logging
+from meta.user import User, Task
+
+
+import_loggers = ["pysondb", "filelock"]
+for loggers in import_loggers:
+    l = logging.getLogger(loggers)
+    l.setLevel(logging.CRITICAL)
+
+del l
+
+l = logging.getLogger("database")
+
+db_logger = Logger(logger=l, filename="database.log", base_level=logging.DEBUG)
 
 DB_FILE_PATH = os.path.join(Path(__file__).parent, "db.json")
+DictType = Dict[str, Union[str, int, List[Task]]]
 
 
 class Database:
     def __init__(self) -> None:
         self._db = db.getDb(DB_FILE_PATH)
+
+    def add_user(self, user: User) -> None:
+        db_logger.log(logging.DEBUG, self.convet_user_object_to_json(user))
+        self._db.add(self.convet_user_object_to_json(user))
+
+    def convet_user_object_to_json(self, user: User) -> DictType:
+        default_json = {"username": "", "user_id": 0, "tasks": []}
+
+        # set all the values to the default json object
+        default_json["username"] = user.username
+        default_json["user_id"] = user.user_id
+
+        for task in user.tasks:
+            default_json["tasks"].append(
+                {
+                    "task_id": task.task_id,
+                    "task_desc": task.task_desc,
+                    "task_date": task.task_date,
+                }
+            )
+
+        return default_json
--- meta/handlers.py
+++ meta/handlers.py
@@ -3,7 +3,17 @@ import uuid
 from collections import deque
 from typing import Deque, Dict, Union, List
 
+from meta.logger import Logger, logging
 from .user import NonUserInfo, Task, User, UserCmd, UserQueueData
+
+q_log = logging.getLogger("UserQueueHandler")
+q_logger = Logger(q_log, base_level=logging.DEBUG, filename="")
+
+ch_log = logging.getLogger("UserCommandHandler")
+ch_logger = Logger(q_log, base_level=logging.DEBUG, filename="")
+
+th_log = logging.getLogger("UserTaskHandler")
+th_logger = Logger(q_log, base_level=logging.DEBUG, filename="")
 
 
 class UserQueueHandler:
@@ -12,11 +22,14 @@ class UserQueueHandler:
         self._q: Deque[Union[UserQueueData, NonUserInfo]] = deque()
 
     def add_user(self, user: UserQueueData) -> None:
+        q_logger.log(logging.DEBUG, message=f"Got user {user}")
         self._q.append(user)
 
     def get_user(self) -> UserQueueData:
         if self._q:
-            return self._q.popleft()
+            user = self._q.popleft()
+            q_logger.log(logging.DEBUG, message=f"Popped user {user}")
+            return user
 
 
 class UserCommandHandler:
@@ -25,10 +38,13 @@ class UserCommandHandler:
         self._c_dict: Dict[int, UserCmd] = {}
 
     def set_user_command(self, user_id: int, cmd: UserCmd) -> None:
+        ch_logger.log(logging.DEBUG, message=f"set user {user_id=}: {cmd=}")
         self._c_dict[user_id] = cmd
 
     def get_user_command(self, user_id) -> Union[UserCmd, None]:
+        ch_logger.log(logging.DEBUG, message=f"get user {user_id=}")
         if user_id in self._c_dict:
+            ch_logger.log(logging.INFO, message=f"get user return for {user_id=}")
             return self._c_dict[user_id]
 
     def remove_old_commands(self, time_limit_s=300) -> List[int]:
@@ -48,13 +64,17 @@ class UserTaskHandler:
         self._tq: Dict[int, Task] = {}
 
     def add_task(self, user_id: int, task_desc: str) -> None:
+        th_logger.log(logging.DEBUG, message=f"add task {user_id=}: {task_desc=}")
         _uuid = int(str(uuid.uuid4())[:10])
         self._tq[user_id] = Task(task_id=_uuid, task_desc=task_desc, task_date="")
 
     def add_date(self, user_id: int, date: str) -> None:
+        th_logger.log(logging.DEBUG, message=f"add date {user_id=}: {date=}")
         self._tq[user_id].task_date = date
 
     def remove_user(self, user_id_list: List[int]) -> None:
+        th_logger.log(logging.DEBUG, "in remove user")
         for _id in user_id_list:
             if _id in self._tq:
+                th_logger.log(logging.INFO, message=f"successfully removed user {_id=}")
                 del self._tq[_id]
